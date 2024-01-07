6d3e450de7e37a648f375e6038ce32c6bee09fb2
krishnachaithanya4716 <krishnachaithanya4716@gmail.com>
2024-01-07 11:54:05 +0530

    added zod validations

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const app = require('express')
+const app = require('express');
+const { createTodo, updateTodo } = require('./types');
 
 
 
@@ -11,12 +12,40 @@ app.use(express.json())
 //adding todo in app
 app.post('addTodo',(req,res)=>{
 
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload)
+
+    if(!parsedPayload.success){
+        res.status(411).json({
+            message:"you have sent wrong request body"
+        })
+    }
+    else{
+        //put it in mongo db
+    }
+
 })
 
 app.put('updTodo',(req,res)=>{
 
+    const updatePayload = req.body;
+    const parsedPayload = createTodo.safeParse(updateTodo)
+
+    if(!parsedPayload.success){
+        res.status(411).json({
+            message:"you have sent wrong id"
+        })
+
+        return;
+    }
+    else{
+        //put it in mongo db
+    }
+
+
+
 })
 
-app.length('getAllTodo',(req,res)=>{
+app.get('getAllTodo',(req,res)=>{
 
 })
